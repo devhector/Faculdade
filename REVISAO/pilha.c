6ff25cd1609292e29c5b0a78f96eda077ed3befd
hectorhu17 <hectorhu17@gmail.com>
2020-12-13 18:46:17 -0300

    create isEmpty function

--- REVISAO/pilha.c
+++ REVISAO/pilha.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct stack{
+
+    int number;
+    struct stack *next;
+
+}Stack;
+
+int isEmpty(Stack **varStack);
+
 int main(){
 
 
 
     return 0;
+}
+
+int isEmpty(Stack **varStack){
+
+    if(varStack == NULL){
+
+        return 1;
+
+    }
+
+    if(*varStack == NULL){
+
+        return 1;
+
+    }
+
+    return 0;
+    
 }
